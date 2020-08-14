cfbcd68b1eb6a2e684c9910053e659c7869a9d23
Josh Matthews <josh@joshmatthews.net>
2020-08-14 18:38:23 -0400

    Only pass one random world.

--- random.sh
+++ random.sh
@@ -5,6 +5,6 @@ set -o pipefail
 
 rm -f image.png download.zip
 cargo run --bin populate download.zip
-world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R`
+world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R | head -n 1`
 cargo run --bin capture image.png ${world}
 open image.png
