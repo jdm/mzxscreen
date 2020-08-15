8cb2a46abb48db57d1e001a12a51379192a0fa10
Josh Matthews <josh@joshmatthews.net>
2020-08-14 21:51:16 -0400

    Support competition archives.

--- random.sh
+++ random.sh
@@ -5,6 +5,9 @@ set -o pipefail
 
 rm -f image.png download.zip
 cargo run --bin populate download.zip
+for archive in `find unzipped -name \*.zip -o -name \*.ZIP`; do
+    unzip -d unzipped ${archive}
+done
 world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R | head -n 1`
 cargo run --bin capture image.png ${world}
 open image.png
