68b58fd5bc91a9c2afebe49e91c5f4c8fc39e06e
Josh Matthews <josh@joshmatthews.net>
2020-08-14 23:39:28 -0400

    Handle subarchives better.

--- random.sh
+++ random.sh
@@ -3,19 +3,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -z "$1" ]
+
+if [ -z "${1+x}" ]
   then
     rm -f image.png download.zip
     cargo run --bin populate download.zip output.json
 fi
-IFS=$'\n'
-for archive in "`find unzipped -name \*.zip -o -name \*.ZIP`"; do
-    if [ -n "${archive}" ]
-      then
-        echo ${archive}
-        unzip -d unzipped "${archive}"
-    fi
-done
+find unzipped \( -name \*.zip -o -name \*.ZIP \) -exec unzip -d unzipped {} \;
 world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R | head -n 1`
 cargo run --bin capture image.png "${world}"
 open image.png
