04466bef8a67a8523116bceb2b598fe82a5f5908
Josh Matthews <josh@joshmatthews.net>
2020-08-14 22:34:00 -0400

    fix files with spaces

--- random.sh
+++ random.sh
@@ -6,8 +6,8 @@ set -o pipefail
 rm -f image.png download.zip
 cargo run --bin populate download.zip output.json
 for archive in `find unzipped -name \*.zip -o -name \*.ZIP`; do
-    unzip -d unzipped ${archive}
+    unzip -d unzipped "${archive}"
 done
 world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R | head -n 1`
-cargo run --bin capture image.png ${world}
+cargo run --bin capture image.png "${world}"
 open image.png
