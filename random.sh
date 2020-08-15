7a8922672225034fdca03182b6a22041d04be5e1
Josh Matthews <josh@joshmatthews.net>
2020-08-14 23:19:59 -0400

    More space filename fixes

--- random.sh
+++ random.sh
@@ -5,7 +5,9 @@ set -o pipefail
 
 rm -f image.png download.zip
 cargo run --bin populate download.zip output.json
-for archive in `find unzipped -name \*.zip -o -name \*.ZIP`; do
+IFS=$'\n'
+for archive in "`find unzipped -name \*.zip -o -name \*.ZIP`"; do
+    echo ${archive}
     unzip -d unzipped "${archive}"
 done
 world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R | head -n 1`
