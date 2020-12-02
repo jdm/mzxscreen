cac819e00c1ec148046384fde22d6739c583a959
Josh Matthews <josh@joshmatthews.net>
2020-12-02 13:40:18 -0500

    Allow specifying boards to capture.

--- random.sh
+++ random.sh
@@ -3,13 +3,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
 if [ -z "${1+x}" ]
   then
     rm -f image.png download.zip
     cargo run --bin populate download.zip output.json
+else
+  shift
 fi
 find unzipped \( -name \*.zip -o -name \*.ZIP \) -exec unzip -d unzipped {} \;
 world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R | head -n 1`
-cargo run --bin capture image.png output.json "${world}"
+echo cargo run --bin capture image.png output.json "${world}" "$@"
+cargo run --bin capture image.png output.json "${world}" "$@"
 open image.png
