6631f7bd30c5ab0f53af60395292d7110b1d7ee7
Josh Matthews <josh@joshmatthews.net>
2021-02-10 23:25:57 -0500

    Don't panic on archives with no worlds.

--- random.sh
+++ random.sh
@@ -13,6 +13,11 @@ else
 fi
 find unzipped \( -name \*.zip -o -name \*.ZIP \) -exec unzip -d unzipped {} \;
 world=`find unzipped -name \*.mzx -o -name \*.MZX | sort -R | head -n 1`
+if [ -z "${world}" ]
+then
+    echo "No world found."
+    exit 1
+fi
 echo cargo run --bin capture image.png output.json "${world}" "$@"
 cargo run --bin capture image.png output.json "${world}" "$@"
 open image.png
